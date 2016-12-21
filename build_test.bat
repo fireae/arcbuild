65da91a3ad7c88b710e56035e46a681a24753729
Lin N.Y <lnychina@gmail.com>
2016-12-21 10:40:08 +0800

    ...

--- build_test.bat
+++ build_test.bat
@@ -4,8 +4,8 @@ DEL /S /Q *.zip
 DEL /S /Q _build
 FOR %%A IN (simple_sdk simple_sdk_with_mpbase multiple_modules_sdk) DO (
     cmake -D_BUILD=ON -DPLATFORM=android -DTYPE=SHARED -DROOT=E:\NDK\android-ndk-r11b -DSOURCE_DIR=examples/%%A -DBINARY_DIR=_build/%%A -DSUFFIX=_%%A -P arcbuild.cmake
-    cmake -D_BUILD=ON -DPLATFORM=vs2015 -DTYPE=SHARED -DSOURCE_DIR=examples/%%A -DBINARY_DIR=_build/%%A -DSUFFIX=_%%A -P arcbuild.cmake
-    cmake -D_BUILD=ON -DPLATFORM=vs2013 -DTYPE=SHARED -DSOURCE_DIR=examples/%%A -DBINARY_DIR=_build/%%A -DSUFFIX=_%%A -P arcbuild.cmake
+    cmake -D_BUILD=ON -DPLATFORM=vs2015  -DTYPE=SHARED -DSOURCE_DIR=examples/%%A -DBINARY_DIR=_build/%%A -DSUFFIX=_%%A -P arcbuild.cmake
+    cmake -D_BUILD=ON -DPLATFORM=vs2013  -DTYPE=SHARED -DSOURCE_DIR=examples/%%A -DBINARY_DIR=_build/%%A -DSUFFIX=_%%A -P arcbuild.cmake
 )
 PUSHD examples\local_arcbuild
 DEL /S /Q _arcbuild
