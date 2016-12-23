c5ac8b599baefc7dfd991ccb6b968b964770f127
Lin N.Y <maxint@foxmail.com>
2016-12-23 20:34:08 +0800

    Update examples

---
+++ examples/combine_modules/demo/demo.c
@@ -0,0 +1,6 @@
+#include <arcsoft_xxx.h>
+
+int main() {
+    ASXXX_Test();
+    return 0;
+}
--- examples/combine_modules/inc/arcsoft_xxx.h
+++ examples/combine_modules/inc/arcsoft_xxx.h
@@ -13,7 +13,6 @@
 #   define ASXXX_DLL
 #endif
 
-//#include "asvloffscreen.h"
 typedef long MLong;
 typedef char MTChar;
 
@@ -21,31 +20,6 @@ typedef char MTChar;
 extern "C" {
 #endif
 
-typedef struct
-{
-    MLong lCodebase;                ///< Codebase version number
-    MLong lMajor;                   ///< major version number
-    MLong lMinor;                   ///< minor version number
-    MLong lBuild;                   ///< Build version number, increasable only
-    MTChar* Version;                ///< version in string form
-    MTChar* BuildDate;              ///< latest build Date
-    MTChar* CopyRight;              ///< copyright
-} ArcSoft_XXX_Version;  ///< Object Tracking Engine Version
-
-//////////////////////////////////////////////////////////////////////////
-/// @brief   The function used to get version information of the library.
-///
-/// @return const ArcSoft_XXX_Version *  [OUT]
-///
-/// @since Version 1.0
-///
-/// @details
-///
-/// @remark
-///
-/// @see
-//////////////////////////////////////////////////////////////////////////
-ASXXX_DLL const ArcSoft_XXX_Version * ArcSoft_XXX_GetVersion();
 ASXXX_DLL void ASXXX_Test();
 
 #ifdef __cplusplus
